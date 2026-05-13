a73ff062c412f290d4e76e936c84ed205f40aed4
Stefano Corelli <stefano.corelli03@gmail.com>
2026-05-13 10:54:30 +0200

    implementata sistema segnalazioni con upload foto con route e pulizia errori

--- code/backend/src/models/segnalazione.js
+++ code/backend/src/models/segnalazione.js
@@ -1,33 +1,34 @@
 const mongoose = require('mongoose');
 
 const segnalazioneSchema = new mongoose.Schema({
-    id: {
-        type: Number,
-        required: [true, 'L\'ID della segnalazione è obbligatorio']
-    }, 
-
+    utenteId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User', 
+        required: true
+    },
     descrizione: { 
         type: String,
         required: [true, 'La descrizione della segnalazione è obbligatoria'], 
         trim: true, 
-        minlength: [20, 'La descrizione deve essere almeno di 20 caratteri']
+        minlength: [20, 'Sii più specifico: la descrizione deve avere almeno 20 caratteri']
     },
+    foto: {
+        type: String, // Qui andrà l'URL o il path del file
+        required: [true, 'La foto della segnalazione è obbligatoria']
+    },
+    
 
     // TO DO: aggiungere obbligo inserimento foto. 
     // Sistema ibrido? L'utente carica la foto dal frontend, 
     // la foto viene salvata in locale e nel database viene salvato solo il link alla foto.
     // Vi convince oppure no? 
     // Alternativa: salvare la foto direttamente nel database ma è poco efficiente in termine di spazio.
-    foto: {
-        type: String,
-        required: [true, 'La foto della segnalazione è obbligatoria']
-    },
-
 
     statoSegnalazione: {
         type: String,
         required: true,
-        enum: ['inviata', ' presa_in_carico', 'in_corso', 'risolta', 'archiviata']
+        enum: ['inviata', 'presa_in_carico', 'in_corso', 'risolta', 'archiviata'],
+        default: 'inviata'
     }
 }, 
 {
