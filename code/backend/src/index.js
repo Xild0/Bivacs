b9aae1aef74fef90f35a5671930726215cc5f20c
Stefano Corelli <stefano.corelli03@gmail.com>
2026-05-13 09:19:58 +0200

    risolti problemi connessione stefano

--- code/backend/src/index.js
+++ code/backend/src/index.js
@@ -1,6 +1,5 @@
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
-
+const dns= require("dns");
+dns.setServers(["8.8.8.8","1.1.1.1"]);
 // importing libraries
 const express = require('express');
 const cors = require('cors');
